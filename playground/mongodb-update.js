81d9cc9dd0130bdab0f6c658b710f9bf876f3ff9
David Hemming <dhemming@odecee.com.au>
2017-02-22 13:50:55 +1100

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if (err) {
+		return console.log('Unable to connect to MongoDB server');
+	}
+	console.log('Connected to MongoDB server');
+
+	// db.collection('Todos').findOneAndUpdate({
+	// 	_id: new ObjectID('58acd99e937d5e1b62c7dd2b')
+	// }, {
+	// 	$set: {
+	// 		completed: true
+	// 	}
+	// }, {
+	// 	returnOriginal: false
+	// }).then((result) => {
+	// 	console.log(result);
+	// });
+
+	db.collection('Users').findOneAndUpdate({
+		_id: new ObjectID('58accc4b677016fde94007a9')
+	}, {
+		$set: {
+			name: 'David'
+		},
+		$inc: {
+			age: 1
+		}
+	}, {
+		returnOriginal: false
+	}).then((result) => {
+		console.log(result);
+	});
+
+	// db.close();
+});
